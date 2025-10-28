fde88b844b341f4456f2870198ad89fb08fc04ad
shobhitdamwala <shobhitdamwala@gmail.com>
2025-10-28 15:11:12 +0530

    second commit

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.use((err, req, res, next) => {
 
 
 
-app.listen(process.env.PORT, "0.0.0.0", () => {
+app.listen(process.env.PORT ||8000, "0.0.0.0", () => {
   console.log(`✅ Server is running on http://0.0.0.0:${process.env.PORT}`);
 });
 dbConnection();
